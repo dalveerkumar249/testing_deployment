d59d4001c245620b00770c2a353817b929f4e499
dalveer <dalveerkumar249>
2024-07-11 11:42:26 +0530

    test5

--- src/gh-action.py
+++ src/gh-action.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('hello from final deployment11111111111')
+        'body': json.dumps('hello from final deployment3333333333333')
     }
